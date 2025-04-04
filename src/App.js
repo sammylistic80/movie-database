e2017797a52e23394222be6e868c888c558c5854
sammylistic80 <enyisamuel80@gmail.com>
2025-04-04 19:21:49 +0100

    Add Bootstrap for styling and refactor App component layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+import MovieList from './components/MoviesList';
 
 const App = () => {
   const [movies, setMovies] = useState([
@@ -8,25 +11,27 @@ const App = () => {
         "imdbID": "tt2075247",
         "Type": "movie",
         "Poster": "https://m.media-amazon.com/images/M/MV5BYTM3OWM5MzEtY2VkYS00YjliLWJkZTQtNGM3OGY2MGY2YTM4XkEyXkFqcGdeQXVyNDk0MDg4NDk@._V1_SX300.jpg"
-    },
-    {
-        "Title": "The Krays: Dead Man Walking",
-        "Year": "2018",
-        "imdbID": "tt7858466",
+      },
+      {
+          "Title": "The Krays: Dead Man Walking",
+          "Year": "2018",
+          "imdbID": "tt7858466",
+          "Type": "movie",
+          "Poster": "https://m.media-amazon.com/images/M/MV5BMzEwNjNjN2EtMDM2Zi00MDg1LWI5NWQtMWVmM2Y1MWQzMzg0XkEyXkFqcGc@._V1_SX300.jpg"
+      },
+      {
+        "Title": "Not in Our Name: Dead Man Walking - The Concert",
+        "Year": "1998",
+        "imdbID": "tt0442819",
         "Type": "movie",
-        "Poster": "https://m.media-amazon.com/images/M/MV5BMzEwNjNjN2EtMDM2Zi00MDg1LWI5NWQtMWVmM2Y1MWQzMzg0XkEyXkFqcGc@._V1_SX300.jpg"
+        "Poster": "https://m.media-amazon.com/images/M/MV5BNmQyOGYzZWYtMTk1OS00YTJjLWJlNzItODFlYTMyZTI5YzU5XkEyXkFqcGdeQXVyODAyNDE3Mw@@._V1_SX300.jpg"
     },
-    {
-        "Title": "And Then One Night: The Making of 'Dead Man Walking'",
-        "Year": "2001",
-        "imdbID": "tt0495573",
-        "Type": "movie",
-        "Poster": "N/A"
-    },
-  ])
+  ]);
   return (
-    <div>
-      <h1>Movie Database</h1>
+    <div className='container-fluid movie-app'>
+      <div className='row'>
+        <MovieList movies={movies} />
+      </div>
     </div>
   );
 }
