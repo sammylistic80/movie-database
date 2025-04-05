44e84b752071481c881ecf81ad0c0312a21df728
sammylistic80 <enyisamuel80@gmail.com>
2025-04-05 12:32:46 +0100

    Refactor movie list component and update movie data

--- src/App.js
+++ src/App.js
@@ -1,38 +1,48 @@
 import React, { useState } from 'react';
-import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
-import MovieList from './components/MoviesList';
+import MovieList from './components/MovieList';
 
 const App = () => {
   const [movies, setMovies] = useState([
-      {
-        "Title": "Man Walking Around the Corner",
-        "Year": "1887",
-        "imdbID": "tt2075247",
-        "Type": "movie",
-        "Poster": "https://m.media-amazon.com/images/M/MV5BYTM3OWM5MzEtY2VkYS00YjliLWJkZTQtNGM3OGY2MGY2YTM4XkEyXkFqcGdeQXVyNDk0MDg4NDk@._V1_SX300.jpg"
-      },
-      {
-          "Title": "The Krays: Dead Man Walking",
-          "Year": "2018",
-          "imdbID": "tt7858466",
-          "Type": "movie",
-          "Poster": "https://m.media-amazon.com/images/M/MV5BMzEwNjNjN2EtMDM2Zi00MDg1LWI5NWQtMWVmM2Y1MWQzMzg0XkEyXkFqcGc@._V1_SX300.jpg"
-      },
-      {
-        "Title": "Not in Our Name: Dead Man Walking - The Concert",
-        "Year": "1998",
-        "imdbID": "tt0442819",
-        "Type": "movie",
-        "Poster": "https://m.media-amazon.com/images/M/MV5BNmQyOGYzZWYtMTk1OS00YTJjLWJlNzItODFlYTMyZTI5YzU5XkEyXkFqcGdeQXVyODAyNDE3Mw@@._V1_SX300.jpg"
-    },
-  ]);
-  return (
-    <div className='container-fluid movie-app'>
-      <div className='row'>
-        <MovieList movies={movies} />
-      </div>
-    </div>
-  );
-}
+
+    {
+      "Title": "Merlin",
+      "Year": "2008–2012",
+      "imdbID": "tt1199099",
+      "Type": "series",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BNmI5NDgyZmQtNDc3YS00Mjg0LThmMzEtZjcyNzczOTJlYWY4XkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+      "Title": "Merlin",
+      "Year": "1998",
+      "imdbID": "tt0130414",
+      "Type": "series",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BZDE5YWExMGMtYTA5ZS00ODNlLTkzMjEtN2U5NDUyYWIxNDZhXkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+      "Title": "Arthur & Merlin: Knights of Camelot",
+      "Year": "2020",
+      "imdbID": "tt7052270",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BMTYxODYwMTMtZDFmNi00YTJmLTliM2ItNjIyZTEyOTYzODY0XkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+      "Title": "Arthur & Merlin",
+      "Year": "2015",
+      "imdbID": "tt4065340",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BYTk0YWMzZTktYmU1MS00NWZkLTk1ZTctZjQ3MzE5MjEzNTM5XkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+      "Title": "Merlin and the War of the Dragons",
+      "Year": "2008",
+      "imdbID": "tt1294699",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BMjAwMDc2NzI4M15BMl5BanBnXkFtZTcwODg3MTEwMg@@._V1_SX300.jpg"
+  },
+]);
+return <div>
+  <MovieList movies={movies} />
+</div>;
+};
+
 export default App;
